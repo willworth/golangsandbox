398751527b19f1413a3446e36a5d35086a6911f3
Will Worth <willworthdev@gmail.com>
2024-01-24 10:32:13 +0100

    sfag

--- 1investmentCalc/profit_calculator.go
+++ 1investmentCalc/profit_calculator.go
@@ -1,34 +1,70 @@
 package main
 
 import (
+	"errors"
 	"fmt"
-	// "math"
+	"os"
 )
 
 func main() {
+	revenue, err := getUserInput("Revenue: ")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 
-	revenue := getUserInput("Revenue: ")
-	expenses := getUserInput("Expenses: ")
-	taxRate := getUserInput("Tax Rate: ")
-	ebt, profit, ratio := calculateFinancials(revenue, expenses, taxRate)
+	expenses, err := getUserInput("Expenses: ")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 
-	fmt.Println(ebt)
-	fmt.Println(profit)
-	fmt.Println(ratio)
+	taxRate, err := getUserInput("Tax Rate: ")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 
+	ebt, profit, ratio, err := calculateFinancials(revenue, expenses, taxRate)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println("EBT:", ebt)
+	fmt.Println("Profit:", profit)
+	fmt.Println("Ratio:", ratio)
+
+	err = storeResults(ebt, profit, ratio)
+	if err != nil {
+		fmt.Println(err)
+	}
 }
 
-func calculateFinancials(revenue, expenses, taxRate float64) (float64, float64, float64) {
+func storeResults(ebt, profit, ratio float64) error {
+	results := fmt.Sprintf("EBT: %.1f\nProfit: %.1f\nRatio: %.3f\n", ebt, profit, ratio)
+	return os.WriteFile("results.txt", []byte(results), 0644)
+}
+
+func calculateFinancials(revenue, expenses, taxRate float64) (float64, float64, float64, error) {
 	ebt := revenue - expenses
 	profit := ebt * (1 - taxRate/100)
+	if profit == 0 {
+		return 0, 0, 0, errors.New("Profit is zero, can't calculate ratio")
+	}
 	ratio := ebt / profit
-	return ebt, profit, ratio
-
+	return ebt, profit, ratio, nil
 }
 
-func getUserInput(infoText string) float64 {
+func getUserInput(infoText string) (float64, error) {
 	var userInput float64
 	fmt.Print(infoText)
-	fmt.Scan(&userInput)
-	return userInput
+	_, err := fmt.Scan(&userInput)
+	if err != nil {
+		return 0, err
+	}
+	if userInput < 0 {
+		return 0, errors.New("Value must be non-negative")
+	}
+	return userInput, nil
 }
--- notes.md
+++ notes.md
@@ -6,3 +6,11 @@ https://pkg.go.dev/std
 packages are made up of modules
 
 https://github.com/mschwarzmueller/golang-course-code
+
+every go file must be part of a package.
+
+if you have both files as part of eg main package, no import statement is required. functions are shared across files, but imports aren´t. eg fmt.
+
+every package must have its own subfolder
+
+only upper case funcs and vars are available in other packages
