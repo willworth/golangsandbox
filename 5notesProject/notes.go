3e6d6c267aacc32ecfc37cad761680927cb88867
Will <will.worth@gmail.com>
2024-01-29 16:22:07 +0100

    type switch

--- 5notesProject/notes.go
+++ 5notesProject/notes.go
@@ -14,6 +14,20 @@ type saver interface {
 	Save() error
 }
 
+type displayer interface {
+	Display()
+}
+
+type outputtable interface {
+	saver
+	displayer
+}
+
+// type outputtable interface {
+// 	Save() error
+// 	Display()
+// }
+
 func main() {
 	title, content := getNoteData()
 	todoText := getUserInput("Todo text: ")
@@ -32,17 +46,49 @@ func main() {
 		return
 	}
 
-	todo.Display()
-	err = saveData(todo)
+	err = outputData(todo)
 	if err != nil {
 		return
 	}
-	userNote.Display()
-	err = saveData(userNote)
+
+	err = outputData(userNote)
 
 	if err != nil {
 		return
 	}
+}
+
+// func printSomething(value interface{}) {  // is the same as:
+func printSomething(value any) {
+
+	intVal, ok := value.(int)
+	if ok {
+		fmt.Println("integer", intVal)
+		return
+	}
+
+	float64Val, ok := value.(float64)
+	if ok {
+		fmt.Println("float64", float64Val)
+		return
+
+	}
+
+	switch value.(type) {
+	case int:
+		fmt.Println("Integer:", value)
+	case float64:
+		fmt.Println("Float:", value)
+	case string:
+		fmt.Println("String:", value)
+	}
+
+	fmt.Println(value)
+}
+
+func outputData(data outputtable) error {
+	data.Display()
+	return saveData(data)
 }
 
 func saveData(data saver) error {
