e920da177e0fde9c28351d646dc612c1f94c6236
Will <will.worth@gmail.com>
2024-01-31 15:24:43 +0100

    init server

---
+++ 10restAPI/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	server := gin.Default()
+
+	server.GET("/events", getEvents)
+
+	server.Run(":8080")
+}
+
+func getEvents(context *gin.Context) {
+	// context.JSON(200, )
+	context.JSON(http.StatusOK, gin.H{"message": "Hello!"})
+}
