80c0ab7a7c8f5edcccd415de5d56b5727d37b248
Will <will.worth@gmail.com>
2024-02-02 16:18:59 +0100

    restrict delete to owner

--- 10restAPI/routes/events.go
+++ 10restAPI/routes/events.go
@@ -77,11 +77,16 @@ func updateEvent(context *gin.Context) {
 		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not extract ID"})
 		return
 	}
-
-	_, err = models.GetEventByID(eventId)
+	userId := context.GetInt64("userId")
+	event, err := models.GetEventByID(eventId)
 
 	if err != nil {
 		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not fetch the event"})
+		return
+	}
+
+	if event.UserID != userId {
+		context.JSON(http.StatusUnauthorized, gin.H{"message": "Not the event creator"})
 		return
 	}
 
